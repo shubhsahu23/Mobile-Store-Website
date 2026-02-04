f96392ba3057b0d070eeb7fcd3134115231fa90c
shubh sahu <shubhsahu2006@gmail.com>
2026-02-04 12:50:54 +0530

    changed settings,py

--- mobile_store/mobile_store/settings.py
+++ mobile_store/mobile_store/settings.py
@@ -57,7 +57,7 @@ ROOT_URLCONF = 'mobile_store.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': ['templates'],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
